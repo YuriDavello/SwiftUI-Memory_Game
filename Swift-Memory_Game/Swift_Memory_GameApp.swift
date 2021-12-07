edf5262823b8d745d806e36227e0dfb9ad1a0977
Yuri Davello <yuridavello@hotmail.com>
2021-12-07 20:31:44 -0300

    Initial Commit

---
+++ Swift-Memory_Game/Swift_Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Memory_GameApp.swift
+//  Swift-Memory_Game
+//
+//  Created by Yuri Davello on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
